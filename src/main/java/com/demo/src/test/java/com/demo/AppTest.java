46c583ddf9cc56976d03e5122229f4440c810986
omkar258 <161976319+omkar258@users.noreply.github.com>
2026-02-07 18:06:53 +0530

    Add initial unit test for App class

---
+++ src/main/java/com/demo/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
